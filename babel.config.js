47a16a8876b6e2346a1ba8dd84600299fa23f434
Sougata Roy <sougata.roy26@yahoo.com>
2025-05-31 16:51:20 +0530

    Project setup

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,23 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+        alias: {
+          tests: ['./tests/'],
+          '@components': './src/components',
+          '@screens': './src/screens',
+          '@assets': './src/assets',
+          '@utils': './src/utils',
+          '@hooks': './src/hooks',
+          '@app-types': './src/types',
+          '@constants': './src/constants',
+          '@navigation': './src/navigation',
+        },
+      },
+    ],
+  ],
 };
