6c24b6bc8af17e245040dc6a6a2966ad79670656
Sougata Roy <sougata.roy26@yahoo.com>
2025-05-31 18:46:05 +0530

    Render screens conditionally with respect to App First Launch value using the hook

--- src/navigation/AuthStack.tsx
+++ src/navigation/AuthStack.tsx
@@ -1,5 +1,6 @@
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {AuthStackParamList} from 'types/navigation_types';
+import useAppFirstLaunch from 'hooks/useAppFirstLaunch';
 import OnboardingScreen from '@screens/OnboardingScreen';
 import SignInScreen from '@screens/SignInScreen';
 import SignUpScreen from '@screens/SignUpScreen';
@@ -7,9 +8,16 @@ import SignUpScreen from '@screens/SignUpScreen';
 const Stack = createNativeStackNavigator<AuthStackParamList>();
 
 const AuthStack = () => {
+  const {isAppFirstLaunched} = useAppFirstLaunch();
+  if (isAppFirstLaunched === null) {
+    return;
+  }
+
   return (
     <Stack.Navigator screenOptions={{headerShown: false}}>
-      <Stack.Screen name="Onboarding" component={OnboardingScreen} />
+      {isAppFirstLaunched && (
+        <Stack.Screen name="Onboarding" component={OnboardingScreen} />
+      )}
       <Stack.Screen name="SignIn" component={SignInScreen} />
       <Stack.Screen name="SignUp" component={SignUpScreen} />
     </Stack.Navigator>
