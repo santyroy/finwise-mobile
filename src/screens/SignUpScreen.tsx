2b7281f7d2b6704cc80c7d1cbcc504b8e43b54a0
Sougata Roy <sougata.roy26@yahoo.com>
2025-06-05 18:05:50 +0530

    Implement SignUp Screen

--- src/screens/SignUpScreen.tsx
+++ src/screens/SignUpScreen.tsx
@@ -1,10 +1,148 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {useState} from 'react';
+import {Pressable, StyleSheet, Text, View} from 'react-native';
+import {SafeAreaView} from 'react-native-safe-area-context';
+import CheckBox from '@react-native-community/checkbox';
+import {Link} from '@react-navigation/native';
+
+import Icon from '@react-native-vector-icons/fontawesome6';
+import {useForm, SubmitHandler} from 'react-hook-form';
+import {zodResolver} from '@hookform/resolvers/zod';
+
+import {Colors} from '@constants/colors';
+import {SignupRequest} from 'types/signup_types';
+
+import PrimaryButton from 'components/PrimaryButton';
+import InputComponent from 'components/InputComponent';
+import ErrorComponent from 'components/ErrorComponent';
+import Header from 'components/Header';
+
+import {SignUpSchema} from '@schema/signupSchema';
+
+import {signup} from 'services/auth';
 
 const SignUpScreen = () => {
+  const [isPasswordHidden, setIsPasswordHidden] = useState<boolean>(true);
+  const [toggleCheckBox, setToggleCheckBox] = useState<boolean>(false);
+  const {
+    control,
+    handleSubmit,
+    formState: {errors},
+  } = useForm<SignupRequest>({resolver: zodResolver(SignUpSchema)});
+
+  const onSubmit: SubmitHandler<SignupRequest> = async data => {
+    try {
+      const response = await signup(data);
+      console.log(response);
+    } catch (error) {
+      console.log('Error from SignUpScreen: ', error);
+    }
+  };
   return (
-    <View style={styles.container}>
-      <Text>SignUp Screen</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View>
+        <Header title="Sign Up" />
+      </View>
+
+      <View style={styles.formContainer}>
+        <View style={styles.formInputContainer}>
+          <InputComponent
+            name="name"
+            placeHolderText="Name"
+            control={control}
+          />
+          <View>{errors.name && <ErrorComponent error={errors.name} />}</View>
+        </View>
+        <View style={styles.formInputContainer}>
+          <InputComponent
+            name="email"
+            placeHolderText="Email"
+            control={control}
+            keyboardType="email-address"
+          />
+          <View>{errors.email && <ErrorComponent error={errors.email} />}</View>
+        </View>
+
+        <View style={styles.formInputContainer}>
+          <View style={styles.positionRelative}>
+            <InputComponent
+              name="password"
+              placeHolderText="Password"
+              control={control}
+              secureTextEntry={isPasswordHidden}
+            />
+            <Pressable
+              style={styles.buttonEye}
+              hitSlop={20}
+              onPress={() => setIsPasswordHidden(!isPasswordHidden)}>
+              {isPasswordHidden ? (
+                <Icon
+                  name="eye-slash"
+                  size={20}
+                  color={Colors.base.light[20]}
+                />
+              ) : (
+                <Icon name="eye" size={20} color={Colors.base.light[20]} />
+              )}
+            </Pressable>
+          </View>
+          <View>
+            {errors.password && <ErrorComponent error={errors.password} />}
+          </View>
+        </View>
+
+        <View style={styles.formInputContainer}>
+          <InputComponent
+            name="mobileNumber"
+            placeHolderText="Mobile Number"
+            control={control}
+            keyboardType="number-pad"
+          />
+          <View>
+            {errors.mobileNumber && (
+              <ErrorComponent error={errors.mobileNumber} />
+            )}
+          </View>
+        </View>
+
+        <View style={styles.termsAndConditionContainer}>
+          <CheckBox
+            disabled={false}
+            value={toggleCheckBox}
+            onValueChange={newValue => {
+              setToggleCheckBox(newValue);
+            }}
+            tintColors={{true: Colors.violet[100], false: Colors.violet[100]}}
+          />
+          <View>
+            <Text>
+              By signing up, you agree to the{' '}
+              <Text style={{color: Colors.violet[100]}}>
+                Terms of Service and Privacy Policy
+              </Text>
+            </Text>
+          </View>
+        </View>
+
+        <PrimaryButton
+          title="Sign Up"
+          btnContainerStyle={styles.marginTop30}
+          onPress={handleSubmit(onSubmit)}
+          disabled={!toggleCheckBox}
+          btnStyle={{
+            backgroundColor: toggleCheckBox
+              ? Colors.violet[100]
+              : Colors.violet[40],
+          }}
+        />
+
+        <View style={styles.linksContainer}>
+          <Text style={styles.signupText}>Don't have an account yet?</Text>
+          <Link screen={'SignIn'} params={{}} style={styles.signupLink}>
+            Sign In
+          </Link>
+        </View>
+      </View>
+    </SafeAreaView>
   );
 };
 
@@ -13,5 +151,47 @@ export default SignUpScreen;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: Colors.base.light[100],
+    paddingHorizontal: 10,
+    paddingVertical: 20,
+  },
+  formContainer: {marginTop: 50, gap: 20},
+  formInputContainer: {gap: 5},
+  positionRelative: {position: 'relative'},
+  buttonEye: {
+    position: 'absolute',
+    top: 15,
+    right: 15,
+    backgroundColor: Colors.base.light[100],
+  },
+  termsAndConditionContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    maxWidth: '90%',
+    gap: 5,
+  },
+  marginTop30: {marginTop: 30},
+  forgotPasswordLink: {
+    color: Colors.violet[100],
+    fontSize: 16,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginTop: 10,
+  },
+  linksContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    gap: 5,
+  },
+  signupText: {
+    color: Colors.base.light[20],
+    fontWeight: '500',
+    fontSize: 16,
+  },
+  signupLink: {
+    color: Colors.violet[100],
+    fontSize: 16,
+    fontWeight: 'bold',
   },
 });
