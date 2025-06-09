a1841a4cf04e46a5cd66a3f970526a97613de92c
Sougata Roy <sougata.roy26@yahoo.com>
2025-06-09 22:45:45 +0530

    Updated LoadingModalComponent

--- src/components/LoadingModalComponent.tsx
+++ src/components/LoadingModalComponent.tsx
@@ -1,19 +1,31 @@
 import {FC} from 'react';
-import {ActivityIndicator, Modal, StyleSheet, Text, View} from 'react-native';
+import {
+  ActivityIndicator,
+  Modal,
+  ModalProps,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
 import {Colors} from '@constants/Colors';
 
-interface LoadingComponentProps {
+interface LoadingModalComponentProps extends ModalProps {
   message?: string;
 }
 
-const LoadingComponent: FC<LoadingComponentProps> = ({
+const LoadingModalComponent: FC<LoadingModalComponentProps> = ({
   message = 'Loading...',
+  ...props
 }) => {
   return (
-    <Modal transparent animationType="slide">
+    <Modal
+      backdropColor={'rgba(0,0,0,0.3)'}
+      animationType="slide"
+      statusBarTranslucent
+      {...props}>
       <View style={styles.container}>
-        <View style={styles.loadingContainer}>
-          <Text style={styles.loadingText}>{message}</Text>
+        <View style={[styles.modalContainer, styles.shadowProp]}>
+          <Text style={styles.modalText}>{message}</Text>
           <ActivityIndicator color={Colors.violet[100]} size={'large'} />
         </View>
       </View>
@@ -21,26 +33,35 @@ const LoadingComponent: FC<LoadingComponentProps> = ({
   );
 };
 
-export default LoadingComponent;
+export default LoadingModalComponent;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    paddingHorizontal: 10,
   },
-  loadingContainer: {
-    minHeight: '20%',
-    width: '50%',
+  modalContainer: {
     backgroundColor: Colors.base.light[100],
     borderRadius: 10,
-    boxShadow: '0px 0px 10px rgba(0,0,0,0.5)',
     justifyContent: 'center',
     alignItems: 'center',
+    paddingVertical: 30,
+    paddingHorizontal: 20,
   },
-  loadingText: {
+  shadowProp: {
+    shadowColor: Colors.base.dark[25],
+    shadowOffset: {width: 5, height: 5},
+    shadowOpacity: 0.5,
+    shadowRadius: 5,
+    elevation: 2,
+  },
+  modalText: {
     fontSize: 18,
-    color: Colors.base.dark[25],
+    color: Colors.violet[100],
     marginBottom: 10,
+    fontWeight: '600',
+    textAlign: 'center',
   },
 });
