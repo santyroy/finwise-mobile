58f4c7f8b57d78fd174dab3d686ef7953f05de01
Sougata Roy <sougata.roy26@yahoo.com>
2025-06-10 20:19:38 +0530

    Add verify OTP schema

--- src/schema/signupSchema.ts
+++ src/schema/signupSchema.ts
@@ -12,3 +12,10 @@ export const SignUpSchema = z.object({
     .string({message: 'Mobile number is mandatory'})
     .regex(/^\d{10}$/, {message: 'Mobile number must be exactly 10 digits'}),
 });
+
+export const VerifySignUpSchema = z.object({
+  otp: z
+    .string({message: 'OTP is mandatory'})
+    .regex(/^\d{6}$/, {message: 'OTP must be 6 digits'}),
+  email: z.string({message: 'Email is required'}).email('Invalid Email'),
+});
