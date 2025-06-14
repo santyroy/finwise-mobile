c2098e206b56a278168cd9b8236b6a3977d91ef7
Sougata Roy <sougata.roy26@yahoo.com>
2025-06-14 17:55:55 +0530

    Remove extra schema rules in signin schema

--- src/schema/signinSchema.ts
+++ src/schema/signinSchema.ts
@@ -1,8 +1,6 @@
 import {z} from 'zod';
 
 export const SignInSchema = z.object({
-  email: z.string({message: 'Email is required'}).email('Invalid Email'),
-  password: z
-    .string({message: 'Password is required'})
-    .min(6, 'Password must be min of 6 characters'),
+  email: z.string({message: 'Email is required'}),
+  password: z.string({message: 'Password is required'}),
 });
