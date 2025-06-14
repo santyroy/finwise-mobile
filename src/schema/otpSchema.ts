7e593e125d4afe3ac19f1450fc94de51882065ff
Sougata Roy <sougata.roy26@yahoo.com>
2025-06-14 23:35:04 +0530

    Add OtpPurpose field into OTP Schema

--- src/schema/otpSchema.ts
+++ src/schema/otpSchema.ts
@@ -1,12 +1,19 @@
 import {z} from 'zod';
+import {OtpPurpose} from '@constants/OtpPurpose';
 
 export const VerifyOTPSchema = z.object({
   otp: z
     .string({message: 'OTP is mandatory'})
     .regex(/^\d{6}$/, {message: 'OTP must be 6 digits'}),
   email: z.string({message: 'Email is required'}).email('Invalid Email'),
+  otpPurpose: z.enum(Object.values(OtpPurpose) as [string, ...string[]], {
+    message: 'OTP purpose is mandatory',
+  }),
 });
 
 export const ResendOTPSchema = z.object({
   email: z.string({message: 'Email is required'}).email('Invalid Email'),
+  otpPurpose: z.enum(Object.values(OtpPurpose) as [string, ...string[]], {
+    message: 'OTP purpose is mandatory',
+  }),
 });
