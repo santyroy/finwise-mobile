129534c40131ce424cba986126343f6e40d4ddc6
Sougata Roy <sougata.roy26@yahoo.com>
2025-06-15 23:37:39 +0530

    Update icon color

--- src/components/BackButton.tsx
+++ src/components/BackButton.tsx
@@ -1,13 +1,19 @@
 import {Pressable, PressableProps, StyleSheet} from 'react-native';
 import React, {FC} from 'react';
 import Icon from '@react-native-vector-icons/fontawesome6';
+import {Colors} from '@constants/Colors';
 
 interface BackButtonProps extends PressableProps {}
 
 const BackButton: FC<BackButtonProps> = ({...props}) => {
   return (
     <Pressable style={styles.headerBackButton} hitSlop={20} {...props}>
-      <Icon name="arrow-left" iconStyle="solid" size={24} />
+      <Icon
+        name="arrow-left"
+        iconStyle="solid"
+        size={24}
+        color={Colors.base.dark[50]}
+      />
     </Pressable>
   );
 };
